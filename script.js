42f03cdaad560f3230675c5af2c93f94030c0c5d
ialmeida <ialmeida.pb@gmail.com>
2021-07-01 10:48:27 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const ash={ 
-    direction: `N`,
-    pokemons:0
-    
+let ash={ 
+    direction:{
+      x:0,
+      y:0,
+    },
+  path:[
+    {
+      direction: 'start',
+      x:0,
+      y:0,
+      catchedPokemons:1,
+
+    },
+  ],  
+  totalPokemons:1,
 };
+
+
 
 
