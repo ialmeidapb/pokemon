75729c22188e4ba449bfad790a59ed281d60c544
ialmeida <ialmeida.pb@gmail.com>
2021-07-02 14:15:22 +0100

    almost done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function catchPokemons(strDirection) {
     },
     travelLog: [
       {
-        direction: "Initial",
+        direction: "Starting Point",
         x: 0,
         y: 0,
         pokemons: 1,
@@ -20,19 +20,19 @@ function catchPokemons(strDirection) {
   for (let i = 0; i < strDirection.length; i++) {
     let direction;
     switch (strDirection[i]) {
-      case "w":
+      case "w" && "W":
         direction = "West";
         ash.position.x -= 1;
         break;
-      case "e":
+      case "e" && "E":
         direction = "East";
         ash.position.x += 1;
         break;
-      case "n":
+      case "n" && "N":
         direction = "North";
         ash.position.y += 1;
         break;
-      case "s":
+      case "s" && "S":
         direction = "South";
         ash.position.y -= 1;
         break;
@@ -71,14 +71,7 @@ function printCoordinate(coord) {
   console.log(`coord: [x:${coord.x}, y:${coord.y}]`);
 }
 
-function printTravelLog(ash) {
-  for (let i = 0; i < ash.travelLog.length; i++) {
-    let log = ash.travelLog[i];
-    console.log(
-      `Movement ${i}: [direction:${log.direction}, x:${log.x}, y:${log.y}, pokemon:${log.pokemons}],`
-    );
-  }
-}
+
 
 function printPokemons(ash) {
   console.log(`Pokemons: ${ash.totalPokemons}`);
@@ -95,6 +88,14 @@ let result = catchPokemons(movements);
 document.getElementById("requested-movements").innerHTML = `<p>Movements:${movements}</p>`
 document.getElementById("total-pokemons").innerHTML = `<p>Catched Pokemons:${result.totalPokemons}</p>`;
 
+
+document.getElementById("atual-position").innerHTML = "";
+  for (let i = 0; i < result.travelLog.length; i++) {
+    let log = result.travelLog[i];
+    document.getElementById(
+      "atual-position"
+    ).innerHTML += `<span>Movement ${i}: [Direction=> ${log.direction} || Actual Position=> x:${log.x}, y:${log.y}]</span><br/>`;
+  }
 
 
 }
