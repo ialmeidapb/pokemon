b29e907a392c612da1ab4e6a3eb0a042d41c94ab
ialmeida <ialmeida.pb@gmail.com>
2021-07-02 16:39:19 +0100

    done

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 function catchPokemons(strDirection) {
   let ash = {
     position: {
       x: 0,
       y: 0,
     },
-    travelLog: [
+    trajectory: [
       {
         direction: "Starting Point",
         x: 0,
@@ -13,26 +12,42 @@ function catchPokemons(strDirection) {
         pokemons: 1,
       },
     ],
-    totalPokemons: 1,
+    PokeBall: 1,
   };
 
   let pokemons = 0;
   for (let i = 0; i < strDirection.length; i++) {
     let direction;
     switch (strDirection[i]) {
-      case "w" && "W":
+      case "w":
         direction = "West";
         ash.position.x -= 1;
         break;
-      case "e" && "E":
+      case "W":
+        direction = "West";
+        ash.position.x -= 1;
+        break;
+      case "e":
         direction = "East";
         ash.position.x += 1;
         break;
-      case "n" && "N":
+      case "E":
+        direction = "East";
+        ash.position.x += 1;
+        break;
+      case "n":
         direction = "North";
         ash.position.y += 1;
         break;
-      case "s" && "S":
+      case "N":
+        direction = "North";
+        ash.position.y += 1;
+        break;
+      case "s":
+        direction = "South";
+        ash.position.y -= 1;
+        break;
+      case "S":
         direction = "South";
         ash.position.y -= 1;
         break;
@@ -40,7 +55,11 @@ function catchPokemons(strDirection) {
 
     let pokemonsInCoordinate = 0;
     if (
-      !existsCoordinateInTravelLog(ash.travelLog, ash.position.x, ash.position.y)
+      !existsCoordinateIntrajectory(
+        ash.trajectory,
+        ash.position.x,
+        ash.position.y
+      )
     ) {
       pokemonsInCoordinate = 1;
     }
@@ -52,52 +71,45 @@ function catchPokemons(strDirection) {
       pokemons: pokemonsInCoordinate,
     };
 
-    ash.totalPokemons += pokemonsInCoordinate;
+    ash.PokeBall += pokemonsInCoordinate;
 
-    ash.travelLog.push(log);
+    ash.trajectory.push(log);
   }
-
 
   return ash;
 }
 
 //verify if ash already passed that possition
 
-function existsCoordinateInTravelLog(travelLog, x, y) {
-  return travelLog.some((entry) => entry.x === x && entry.y === y);
+function existsCoordinateIntrajectory(trajectory, x, y) {
+  return trajectory.some((entry) => entry.x === x && entry.y === y);
 }
 
 function printCoordinate(coord) {
   console.log(`coord: [x:${coord.x}, y:${coord.y}]`);
 }
 
-
-
 function printPokemons(ash) {
-  console.log(`Pokemons: ${ash.totalPokemons}`);
+  console.log(`Pokemons: ${ash.PokeBall}`);
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", (event) => {
+  let movements = document.getElementById("movements-input").value;
+  let result = catchPokemons(movements);
 
-let movements = document.getElementById("movements-input").value;
-let result = catchPokemons(movements);
+  document.getElementById(
+    "requested-movements"
+  ).innerHTML = `<p>Movements:${movements}</p>`;
+  document.getElementById(
+    "pokeball"
+  ).innerHTML = `<p>PokeBall Status (Pokemon Catched):${result.PokeBall}</p>`;
 
-
-
-document.getElementById("requested-movements").innerHTML = `<p>Movements:${movements}</p>`
-document.getElementById("total-pokemons").innerHTML = `<p>Catched Pokemons:${result.totalPokemons}</p>`;
-
-
-document.getElementById("atual-position").innerHTML = "";
-  for (let i = 0; i < result.travelLog.length; i++) {
-    let log = result.travelLog[i];
+  document.getElementById("atual-position").innerHTML = "";
+  for (let i = 0; i < result.trajectory.length; i++) {
+    let log = result.trajectory[i];
     document.getElementById(
       "atual-position"
     ).innerHTML += `<span>Movement ${i}: [Direction=> ${log.direction} || Actual Position=> x:${log.x}, y:${log.y}]</span><br/>`;
   }
-
-
-}
-
-);
+});
